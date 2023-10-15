4627069f652aada1dfb2cd8c9527a764d5ca3822
PratyushSoni <pratyushsoni.bt21ele@pec.edu.in>
2023-10-15 22:41:39 +0000

    changes in pages

--- stumanager/app/register/page.tsx
+++ stumanager/app/register/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { useState } from "react";
 import NsForm0 from "../components/new-student/NsForm0"
-import HomeForm from "./Components/HomeForm"
 const data = {
     "sid": null,
+    "age": null,
     "step": null,
     "name": null,
     "gender": null,
@@ -22,14 +22,43 @@ const data = {
     "scholar": null
 }
 
-const Register = ()=>{
-    const [formData, setData]=useState()
+const Register = () => {
+    const [formData, setData] = useState<any>(data)
+    const handleSubmit = ()=>{
+
+        # 
+
+    }
     return (
         <div className='bg-white pt-20' style={{ display: "flex", flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', height: "120vh", gap: '30px' }}>
-                <NsForm0 />
+            <div>
+                <h1 style={{ color: "black", fontSize: "2rem", fontWeight: "bold" }}>
+                    Personal Details
+                </h1>
+                <h1>Please Enter Your Student ID</h1>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text" style={{ color: "black" }}>
+                            <strong>Student ID</strong>
+                        </span>
+                    </label>
+                    <input
+                        value={formData.sid === 0 ? '' : String(formData.sid ?? 0)}
+                        onChange={(e) => { setData({ ...formData, sid: Number(e.target.value) }) }}
+                        type="text"
+                        placeholder="Type here"
+                        className="input input-bordered w-full max-w-xs bg-white"
+                    />
+                </div>
+                <button
+                    className="btn btn-active btn-success hover:bg-green-600"
+                onClick={handleSubmit}
+                >
+                    Start Registration
+                </button>
             </div>
-        
-        // <HomeForm data={data} />
+        </div>
+
     )
 }
 
